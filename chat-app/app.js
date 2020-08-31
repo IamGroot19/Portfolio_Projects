b002a54b3d99ce28fe95e5aa68aa30f18a8196cc
Bharath <bharathram1998@gmail.com>
2020-09-01 00:48:15 +0530

    Include socket variables in app.js & include socket.io.js script in chat.html
    Inside the app.js create server, io variables. Under the hood expressJS uses createServer(). We directly use createServer from node coz we need to combine it with socket.io later on.

--- chat-app/app.js
+++ chat-app/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
-let app = express();
+const path = require('path')
+const http = require('http'); 
+const socketio = require('socket.io');
 
+// express under the hood uses createServer(). We directly use createServer from node coz we need to combine t with socket.io later on
+const app = express();
+const server = http.createServer(app); 
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+
+//Run when client connects
+io.on('connection', (socket0 => {
+    console.log(' New connection ');
+}));
 
 app.listen( process.env.PORT || 3000, () => {
     console.log("Server started running...");
