6fdfa7a8d49f78acd6e4823e9e8940e9af2d7b12
Bharath <bharathram1998@gmail.com>
2020-09-09 23:48:05 +0530

    Add a new folder for a new project - "Trained Deep Learning Models for inference"

--- chat-app/utils/messages.js
+++ chat-app/utils/messages.js
@@ -1,11 +1,11 @@
 // moment package gives us date  &time
-const moment = require('moment');
-
+const moment = require('moment-timezone');
+moment.tz.setZone
 function formatMessage( username, text) {
     return {
         username,
         text,
-        time: moment().format('h:m a')  
+        time: moment.tz("Asia/Kolkata").format('DD MMM YYYY, hh:mm a')  
     };
 }
 
