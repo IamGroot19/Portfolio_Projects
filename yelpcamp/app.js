e608edfe72a008c566cb1732cddec047c3428735
Bharath <bharathram1998@gmail.com>
2020-09-10 00:27:42 +0530

    Setup a basic structure for landing page and another page for campgrounds.
    Setup campgrounds.ejs (contains list of all camp sites) and landingPage.ejs (which is THE landing page of the website).
    Databases are not yet setup, even the camp data is hardcoded for timebeing.

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -5,9 +5,19 @@ app.set("view engine", "ejs");
 
 /// Setting up basic routes
 app.get("/", (req,res) => {
-    res.send("Landing Page");
+    res.render("landingPage");
 });
 
+app.get("/campgrounds", (req,res) => {
+    
+    let camps = [
+        { name: "Tada" , image:" https://www.photosforclass.com/download/px_699558" },
+        { name: "Ooty", image:"https://www.photosforclass.com/download/px_1840421"},
+        { name: "Moonar", image: "https://www.photosforclass.com/download/px_712067"}
+    ]
+
+    res.render("campgrounds.ejs", {camps:camps}); 
+})
 PORT = process.env.PORT || 6969; 
 app.listen(PORT, process.env.IP, () => {
     console.log("Yelpcamp server started on Port ", PORT, "...");
